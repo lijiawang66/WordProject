a5b3eb0b81d4d92ae7c1704706e7f23fc4085c09
lijiawang66 <752151900@qq.com>
2016-12-30 11:24:49 +0800

    初始化

---
+++ src/app.js
@@ -0,0 +1,18 @@
+require('./styles/app.scss');
+
+//
+// var string = require('./scripts/common');
+// console.log(string);
+
+
+//import Name from './scripts/common';
+//console.log(Name);
+
+ var coon = require('./scripts/common');
+//
+ coon.getList();
+
+// import d from './scripts/es6.js';
+
+// console.log(d);
+// console.log(ES6);
